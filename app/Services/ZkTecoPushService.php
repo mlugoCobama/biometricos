7aecf661947be2f03f7bae982371f70704a5a847
miguellugo30 <ingmchlugo@gmail.com>
2026-08-27 17:33:17 -0600

    Sincronizacion de hora 2

--- app/Services/ZkTecoPushService.php
+++ app/Services/ZkTecoPushService.php
@@ -409,7 +409,7 @@ class ZkTecoPushService
         return DeviceCommand::create([
             'device_id' => $device->id,
             'command_type' => 'SET_TIME',
-            'command_text' => "SET TIME {$nowStr}",
+            'command_text' => "DATA UPDATE OPTION SetTime={$nowStr}",
             'status' => 'pending',
         ]);
     }
